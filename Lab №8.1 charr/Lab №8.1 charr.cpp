ad46dca7a5ec837377d15f2c38e7839d8b7bea39
Volodya <chuchman002@gmail.com>
2023-11-30 23:23:31 +0200

    Add project files.

---
+++ Lab №8.1 charr/Lab №8.1 charr.cpp
@@ -0,0 +1,58 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+bool containsAllLetters(const char input[]) {
+    const char* target = "while";
+
+    for (size_t i = 0; i < strlen(target); ++i) {
+        bool found = false;
+        for (size_t j = 0; j < strlen(input); ++j) {
+            if (target[i] == input[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            return false;
+        }
+    }
+    return true;
+}
+
+char* replaceWhile(char* input) {
+    char* pos = strstr(input, "while");
+
+    while (pos != nullptr) {
+        memmove(pos + 2, pos + 5, strlen(pos + 5) + 1);
+        memcpy(pos, "**", 2);
+        pos = strstr(pos + 2, "while");
+    }
+
+    return input;
+}
+
+int main() {
+    const int max_length = 100;
+    char inputString[max_length];
+
+    cout << "Print line: ";
+    cin.getline(inputString, max_length);
+
+    if (containsAllLetters(inputString)) {
+        cout << "String contains all letters from 'while'.\n";
+    }
+    else {
+        cout << "String doesn`t contain all letters with 'while'.\n";
+    }
+
+    char* result = replaceWhile(inputString);
+
+    cout << "Result: " << result << endl;
+
+    delete[] result;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab №8.1 charr/Lab №8.1 charr.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestContainsAllLetters_Negative)
+		{
+			const char* testInput = "random text"; 
+
+			// Перевіряємо, чи функція containsAllLetters повертає false для рядка, який не містить усі літери з "while"
+			Assert::IsFalse(containsAllLetters(testInput));
+		}
+	};
+}
